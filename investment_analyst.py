6e059028a956fa53ef4f0c96a5f50a810ab34215
ashaysawant <amazingashay@gmail.com>
2024-09-27 13:56:17 +0530

    Update investment_analyst.py
    added yfinance integration

--- investment_analyst.py
+++ investment_analyst.py
@@ -5,6 +5,10 @@ import requests
 from streamlit_extras.stylable_container import stylable_container
 import logging
 from botocore.exceptions import ClientError
+import re
+import yfinance as yf
+import pandas_ta as ta
+from datetime import datetime, timedelta
 # import pandas as pd
 # import numpy as np
 
@@ -15,24 +19,19 @@ questions = ["What is Microsoft's expected revenue and earnings growth?",
                  "Summarize Apple's latest earnings report."]
 
 PROMPT_TEMPLATE2 = """
-System: You are an Investment Analyst AI system, and provides answers to questions by using fact based and statistical information when possible. 
-Use the following pieces of information to provide a concise answer to the question enclosed in <question> tags. 
-
+System: You are an expert Investment Analyst, and your mastery encompasses analyzing both stock fundamentals and intricate technical indicators.
+You possess the ability to decode complex market dynamics, providing clear insights and recommendations backed by a thorough understanding of interrelated factors. 
+As a TA authority, your role is to decipher market trends, make informed predictions, and offer valuable perspectives.
+User's question enclosed in <question> tags. Relevent company's ticker symbol is given in <ticker> tags.
 If the question is not related to financial information, politely inform the user that you can only answer related to financial questions.
 Always provide a recommendation to buy the stock of the company or not, given the information available, 
 give brutally honest opinions if you think we should buy it tell us to buy
-and also be honest with it when you think we shouldn't buy, by the numbers available
+and also be honest with it when you think we shouldn't buy, by the numbers available.
 also try to analyse if we should invest in short term or long term. 
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
-
-<question>
-{question}
-</question>
-
+<question>{question}</question>
+<ticker>{ticker}</ticker>
 The response should be specific and use statistics or numbers when possible.
 """
-#Also give reference to your souces.
-
 
 class ComprehendDetect:
     """Encapsulates Comprehend detection functions."""
@@ -116,6 +115,44 @@ def get_ticker(company_name):
     company_code = data['quotes'][0]['symbol']
     return company_code
 
+def get_market_data(company_symbols):
+    end_date = datetime.today()
+    start_date = end_date - timedelta(days=120)  # 4 months before today
+    summary = []
+    for company_symbol in company_symbols:
+        # Fetch stock data using yfinance
+        stock_data = yf.download(company_symbol, start=start_date, end=end_date)
+        # Calculate technical indicators using pandas-ta
+        stock_data.ta.macd(append=True)
+        stock_data.ta.rsi(append=True)
+        stock_data.ta.bbands(append=True)
+        stock_data.ta.obv(append=True)
+
+        # Calculate additional technical indicators
+        stock_data.ta.sma(length=20, append=True)
+        stock_data.ta.ema(length=50, append=True)
+        stock_data.ta.stoch(append=True)
+        stock_data.ta.adx(append=True)
+
+        # Calculate other indicators
+        stock_data.ta.willr(append=True)
+        stock_data.ta.cmf(append=True)
+        stock_data.ta.psar(append=True)
+
+        #convert OBV to million
+        stock_data['OBV_in_million'] =  stock_data['OBV']/1e7
+        stock_data['MACD_histogram_12_26_9'] =  stock_data['MACDh_12_26_9'] # not to confuse chatGTP
+
+        # Summarize technical indicators for the last day
+        last_day_summary = stock_data.iloc[-1][['Adj Close',
+            'MACD_12_26_9','MACD_histogram_12_26_9', 'RSI_14', 'BBL_5_2.0', 'BBM_5_2.0', 'BBU_5_2.0','SMA_20', 'EMA_50','OBV_in_million', 'STOCHk_14_3_3', 
+            'STOCHd_14_3_3', 'ADX_14',  'WILLR_14', 'CMF_20', 
+            'PSARl_0.02_0.2', 'PSARs_0.02_0.2'
+        ]]
+        summary.append(last_day_summary)
+        
+    return summary
+
 def generate_prompt(question):
     LLM_PROMPT = PROMPT_TEMPLATE2.format(question=question)
     qa_prompt = LLM_PROMPT.format(question=question)
@@ -138,21 +175,29 @@ def get_bot_response(userInput):
             Payload=json.dumps(request_body)
         )
         response = json.loads(lambda_response['Payload'].read().decode('utf-8'))
-        print(response)
+        # print(response)
         api_json = response['body']
-        # bot_response = api_json["output"]["result"].replace('$','\\$').replace('\[\d*\]','')
-        bot_response = api_json["output"].replace('$','\\$')
+
+        bot_response = json.loads(api_json["output"].replace("$","\$"))
+        print(bot_response)
+        bot_response = bot_response["result"]
+        final_resp = re.sub('%\[\d\]%','',bot_response)
+        print(final_resp)
+        # bot_response = api_json["output"].replace('$','\\$')
+        # bot_response = json.loads(api_json["output"].replace('$','\\$'))
+        # bot_response = bot_response["result"].replace('%\[\d\]%','')    
+        # print(bot_response)
         
     except requests.exceptions.RequestException as e:
         print(f'Error: {e}')
-        bot_response = 'Sorry, something went wrong. Please try again later.'
+        final_resp = 'Sorry, something went wrong. Please try again later.'
 
     if "ia_sessionId" in api_json.keys():
         st.session_state['ia_session_id'] = api_json["sessionId"]
     if "citations" in api_json.keys(): 
         citations = api_json["citations"]
-        st.session_state.ia_messages.append({"role": "assistant", "content": bot_response, "citations":citations})
-    return bot_response
+        st.session_state.ia_messages.append({"role": "assistant", "content": final_resp, "citations":citations})
+    return final_resp
 
 # Function to store user information
 def store_user_info(chat_history):
@@ -226,7 +271,6 @@ def app():
     row = st.columns([1,3,2])
 
     with row[0]:
-        # with st.expander("Most frquently asked questions:"):
         with  stylable_container(
             "codeblock",
             """
@@ -235,7 +279,6 @@ def app():
             }
             """,
         ):
-        # st.container(height=500):
             st.write("Most frquently asked questions:")
             for question in questions:
                 st.code(question,language=None)
@@ -275,7 +318,14 @@ def app():
     if userInput:
         st.session_state.ia_messages.append({"role": "user", "content": userInput,"citations":None})
         update_chat_messages(ctr)
-        tickers = get_company_symbols(userInput)
+        with market_info:
+            with st.spinner('Analyzing question and information :'):
+                tickers = get_company_symbols(userInput)
+            with st.spinner('Loading market information...'):
+                summary = get_market_data(tickers)
+            for ticker in tickers:
+                market_info.write("Summary for {}:".format(ticker))
+                market_info.write(summary[tickers.index(ticker)])
         
         if tickers:
             st.session_state.tickers = tickers
